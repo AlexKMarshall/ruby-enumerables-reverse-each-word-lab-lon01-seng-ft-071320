5756b00ffe1dad1a83ef8d63285c37ecc7ca7d98
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-23 17:24:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   split_sentence = sentence.split(" ")
   reversed = []
   split_sentence.each do |word|
-    
+    reversed << word.reverse
   end
-    
+  reversed.join
 end
